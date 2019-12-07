1cd14763d0f47342ee3cfc5adab75a5a317abb18
Jason Antman <jason@jasonantman.com>
2019-12-07 17:12:08 -0500

    remove pandolf and clean up output

--- README.md
+++ README.md
@@ -16,20 +16,13 @@ GPS tracks and other info from my hikes
 
 ## Calorie Calculations and Inaccuracies
 
-For estimated calorie consumption over hikes, this script outputs values from two different equations: the commonly-used Pandolf [^1] equation with the decline extension developed by Santee [^2] (and using terrain factors from [^3]), and the modern equation developed by Ludlow and Weyand [^4].
+For estimated calorie consumption over hikes, this script outputs values from the modern equation developed by Ludlow and Weyand [^1].
 
 While these formulas are scientifically rigorous, the way they're applied in this script is definitely not and should be considered a relatively rough estimate. Among the more troublesome points with my method of applying these formulas to data collected in uncontrolled field conditions with the type of GPS logging commonly used by hikers are:
 
 1. At best, I weigh myself and my pack before a hike and the pack after. At worst, I may only weigh before or after and estimate the pack's starting or ending weight. The calculations performed by this program assume a constant linear decrease in pack weight over the course of the hike, with weight being decremented from starting to ending at each point in the GPS track. Assuming a short day hike where only water is taken out of the pack, this shouldn't be too inaccurate, though it certainly does skew the results especially for hikes that mix flat and steep terrain along with increased water intake on difficult terrain.
 2. The grade/slope is taken from GPS elevation information with no supplemental topographical references. In addition to the inherent inaccuracies in GPS data, this also means that GPS sampling rate can have a large impact on the results; a GPS sampling rate of exactly the same time it takes you to cross a mountain could result in a calculated 0% grade.
-3. The Pandolf (and Santee variant) equation relies on a terrain factor to account for increased work crossing various types of terrain (essentially from a paved road at one end of the spectrum to a swamp at the other). This program currently only accepts a single terrain factor for the whole hike/track.
 
 ## Footnotes
 
-[^1]: https://www.ncbi.nlm.nih.gov/pubmed/908672 Pandolf KB, Givoni B, Goldman RF: Predicting energy expenditure with loads while standing or walking very slowly. J Appl Physiol Respir Environ Exerc Physiol 1977; 43(4): 577–81.
-
-[^2]: Santee WR, Blanchard LA, Speckman KL, Gonzalez JA, Wallace RF: Load Carriage Model Development and Testing with Field Data. Technical Note. Natick, MA, U.S. Army Research Institute of Environmental Medicine, Report No.: ADA#415788, 2003.
-
-[^3]: https://www.researchgate.net/publication/284162748_TERRAIN_FACTORS_FOR_PREDICTING_WALKING_AND_LOAD_CARRIAGE_ENERGY_COSTS_REVIEW_AND_REFINEMENT Richmond, Paul & Potter, Adam & Santee, William. (2015). TERRAIN FACTORS FOR PREDICTING WALKING AND LOAD CARRIAGE ENERGY COSTS: REVIEW AND REFINEMENT. Journal of Sport and Human Performance. 3. 10.12922/jshp.0067.2015.
-
-[^4]: https://www.physiology.org/doi/full/10.1152/japplphysiol.00504.2017 Walking economy is predictably determined by speed, grade, and gravitational load. Lindsay W. Ludlow and Peter G. Weyand. Journal of Applied Physiology 2017 123:5, 1288-1302
+[^1]: https://www.physiology.org/doi/full/10.1152/japplphysiol.00504.2017 Walking economy is predictably determined by speed, grade, and gravitational load. Lindsay W. Ludlow and Peter G. Weyand. Journal of Applied Physiology 2017 123:5, 1288-1302
--- convert.py
+++ convert.py
@@ -22,47 +22,22 @@ logger = logging.getLogger()
 class GpxCalories(object):
 
     def __init__(
-        self, gpx, weight_lbs, pack_start_lbs, pack_end_lbs, terrain_factor,
-        rmr
+        self, gpx, weight_lbs, pack_start_lbs, pack_end_lbs, rmr
     ):
         """
         Initialize GpxCalories estimator.
-
-        Terrain factor values suggested by the interactive version of this
-        script were taken from
-        https://www.researchgate.net/publication/284162748_TERRAIN_FACTORS_FOR_
-        PREDICTING_WALKING_AND_LOAD_CARRIAGE_ENERGY_COSTS_REVIEW_AND_REFINEMENT
-
-        Richmond, Paul & Potter, Adam & Santee, William. (2015).
-        TERRAIN FACTORS FOR PREDICTING WALKING AND LOAD CARRIAGE ENERGY COSTS:
-        REVIEW AND REFINEMENT. Journal of Sport and Human Performance.
-        3. 10.12922/jshp.0067.2015.
-
-        And are:
-
-        * Paved road: 1.0
-        * Gravel/dirt road: 1.2
-        * Mud: 1.5
-        * Wet clay/ice: 1.7
-        * Sand: 2.0 (approximate)
-        * Swamp: 3.5
-
-        I generally use 1.2 for everything, and assume that time and slope will
-        handle terrain variation.
 
         :param gpx: GPX object containing the data we need. Not required if
           ``gpx_path`` is provided.
         :param weight_lbs: Hiker's weight in pounds.
         :param pack_start_lbs: Pack starting weight in pounds.
         :param pack_end_lbs: Pack ending weight in pounds.
-        :param terrain_factor: Terrain factor for Pandolf equation.
         :param rmr: Resting Metabolic Rate in ml O2/kg/min
         """
         self.gpx = gpx
         self.body_mass = weight_lbs * 0.45359237
         self.pack_start_mass = pack_start_lbs * 0.45359237
         self.pack_end_mass = pack_end_lbs * 0.45359237
-        self.terrain_factor = terrain_factor
         self.rmr = rmr
 
     def run(self, downsample=1):
@@ -72,38 +47,11 @@ class GpxCalories(object):
         According to some random sources on the Internet, the average adult
         should burn approximately 440 cal/hour hiking. For a ballpark.
         """
-        """
-        cloned_gpx = self.gpx.clone()
-        cloned_gpx.reduce_points(2000, min_distance=10)
-        cloned_gpx.smooth(vertical=True, horizontal=True)
-        cloned_gpx.smooth(vertical=True, horizontal=False)
-        moving_time, stopped_time, moving_distance, stopped_distance, \
-        max_speed_ms = cloned_gpx.get_moving_data()
-        ud = self.gpx.get_uphill_downhill()
-        elev = self.gpx.get_elevation_extremes()
-        print({
-            'track_start': self.gpx.get_time_bounds().start_time,
-            'track_end': self.gpx.get_time_bounds().end_time,
-            'duration_sec': self.gpx.get_duration(),
-            'num_points': self.gpx.get_points_no(),
-            'moving_time': moving_time,
-            'stopped_time': stopped_time,
-            'moving_distance': moving_distance,
-            'stopped_distance': stopped_distance,
-            'max_speed_ms': max_speed_ms,
-            '2d_horizontal_distance': self.gpx.length_2d(),
-            'total_elev_inc': ud.uphill,
-            'total_elev_dec': ud.downhill,
-            'min_elev': elev.minimum,
-            'max_elev': elev.maximum
-        })
-        """
         mass_loss_per_point = (
             self.pack_start_mass - self.pack_end_mass
         ) / self.gpx.get_points_no()
         pack_mass = self.pack_start_mass
         last_point = None
-        cal_pandolf = 0
         cal_ludlow = 0
         for point, _, _, idx in gpx.walk():
             if last_point is None:
@@ -132,48 +80,11 @@ class GpxCalories(object):
                 'elev_gain=%s meters; grade=%s%%',
                 distance, time_diff, elev_gain, grade
             )
-            cal_pandolf += self.pandolf(
-                pack_mass, speed, grade, time_diff
-            )
             cal_ludlow += self.ludlow_weyand(
                 grade, speed, time_diff
             )
             last_point = point
-        print('Pandolf calories: %s' % cal_pandolf)
-        print('Ludlow Weyand calories: %s' % cal_ludlow)
-
-    def pandolf(self, pack_kg, speed_ms, grade_pct, duration_sec):
-        logger.debug(
-            'pack_kg=%s speed_ms=%s grade_pct=%s duration_sec=%s',
-            pack_kg, speed_ms, grade_pct, duration_sec
-        )
-        hours = duration_sec / 60.0 / 60.0
-        W = self.body_mass
-        L = pack_kg
-        n = self.terrain_factor
-        G = grade_pct
-        V = speed_ms
-        watts = (
-            1.5 * W + 2.0 * (W + L) * pow(L / W, 2) +
-            n * (W + L) * (1.5 * pow(V, 2) + 0.35 * V * G)
-        )
-        cf = 0
-        if G < 0:
-            # add Santee correction factor for negative grade
-            cf = (
-                n * (
-                    (G * (W + L) * V) / 3.5 -
-                    ((W + L) * pow(G + 6.0, 2) / W) +
-                    (25.0 * pow(V, 2))
-                )
-            )
-        calories_per_hour = (watts + cf) / 4184.0 * 60.0 * 60.0
-        logger.debug(
-            'Grade is %s; watts=%s add Santee cf=%s; final=%s; calories per '
-            'hour=%s hours=%s',
-            grade_pct, watts, cf, watts + cf, calories_per_hour, hours
-        )
-        return calories_per_hour * hours
+        return cal_ludlow
 
     def ludlow_weyand(self, G, V, duration_sec):
         v_O2_rest = self.rmr
@@ -218,9 +129,6 @@ def parse_args(argv):
                    type=float, default=None, help='Pack start weight in pounds')
     p.add_argument('-P', '--pack-end', dest='pack_end', action='store',
                    type=float, default=None, help='Pack end weight in pounds')
-    p.add_argument('-t', '--terrain-factor', dest='terrain', action='store',
-                   type=float, default=None,
-                   help='Terrain factor (for Pandolf equation)')
     p.add_argument('-H', '--height', dest='height', action='store',
                    type=float, default=None, help='Height in cm')
     p.add_argument('-a', '--age', dest='age', action='store',
@@ -245,28 +153,6 @@ def parse_args(argv):
         args.pack_start = float(input('Pack start weight in pounds: ').strip())
     if args.pack_end is None:
         args.pack_end = float(input('Pack end weight in pounds: ').strip())
-    if args.terrain is None:
-        """
-        These values were taken from
-        https://www.researchgate.net/publication/284162748_TERRAIN_FACTORS_FOR_
-        PREDICTING_WALKING_AND_LOAD_CARRIAGE_ENERGY_COSTS_REVIEW_AND_REFINEMENT
-
-        Richmond, Paul & Potter, Adam & Santee, William. (2015).
-        TERRAIN FACTORS FOR PREDICTING WALKING AND LOAD CARRIAGE ENERGY COSTS:
-        REVIEW AND REFINEMENT. Journal of Sport and Human Performance.
-        3. 10.12922/jshp.0067.2015.
-        """
-        sys.stderr.write('Paved road: 1.0\n')
-        sys.stderr.write('Gravel/dirt road: 1.2\n')
-        sys.stderr.write('Mud: 1.5\n')
-        sys.stderr.write('Wet clay/ice: 1.7\n')
-        sys.stderr.write('Sand: 2.0\n')  # approximate
-        sys.stderr.write('Swamp: 3.5\n')
-        sys.stderr.write(
-            'I generally use 1.2 for everything, and assume that time and '
-            'slope will handle terrain variation.\n'
-        )
-        args.terrain = float(input('Terrain factor: ').strip())
     if args.rmr is None:
         # using the Mifflin-St Jeor formula
         rmr = (10 * args.weight) + (6.25 * args.height) - (5 * args.age)
@@ -311,13 +197,15 @@ def set_log_level_format(level, format):
 
 
 if __name__ == "__main__":
+    with open('cmdline.json', 'w') as fh:
+        fh.write(json.dumps(sys.argv[1:]))
     args = parse_args(sys.argv[1:])
     # set logging level
     if args.verbose > 1:
         set_log_debug()
     elif args.verbose == 1:
         set_log_info()
-    files = list(glob.glob('./*.json'))
+    files = list(glob.glob('./20*.json'))
     assert len(files) == 1
     gpx_path = files[0].replace('.json', '.gpx')
     if os.path.exists(gpx_path):
@@ -337,12 +225,16 @@ if __name__ == "__main__":
             fh.write(stats_text)
         print(stats_text)
     cls = GpxCalories(
-        gpx, args.weight, args.pack_start, args.pack_end, args.terrain, args.rmr
+        gpx, args.weight, args.pack_start, args.pack_end, args.rmr
     )
-    cls.run()
-    cls.run(downsample=2)
-    cls.run(downsample=3)
-    cls.run(downsample=4)
-    cls.run(downsample=5)
-    cls.run(downsample=8)
-    cls.run(downsample=10)
+    calories = {0: round(cls.run())}
+    s = f'Estimated calories burned at original intervals: {calories[0]}\n'
+    for n in [2, 3, 4, 5, 8, 10, 12, 20]:
+        calories[n] = round(cls.run(downsample=n))
+        s += 'Estimated calories burned downsampled to every ' \
+             f'{n} points: {calories[n]}\n'
+    with open('calories.json', 'w') as fh:
+        fh.write(json.dumps(calories))
+    with open('calories.txt', 'w') as fh:
+        fh.write(s)
+    print(s)
